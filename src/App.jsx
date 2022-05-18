c2779dbfd844a9ad5b58d3d3169f91cc4df56fb5
Bonamente <56506035@protonmail.com>
2022-05-18 15:41:01 +0500

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './components/layout/Layout.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+
 const App = () => (
-  <div>Hello world!</div>
+  <Layout>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </Layout>
 );
 
 export default App;
