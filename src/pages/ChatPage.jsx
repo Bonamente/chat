f29da5dd96644ce3a8e24a51bf4be6300532efc2
Bonamente <56506035@protonmail.com>
2022-05-23 13:52:16 +0500

    feat: add ChatPage

---
+++ src/pages/ChatPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const ChatPage = () => (
+  <Container className="text-center pt-5">
+    <h1>Welcome!</h1>
+  </Container>
+);
+
+export default ChatPage;
