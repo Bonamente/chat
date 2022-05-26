cc96cd42b0d369d66a793a7b7c3589911113c21c
Bonamente <56506035@protonmail.com>
2022-05-26 15:52:44 +0500

    feat: add new routes

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,6 @@ export default {
   channelsPath: () => [host, prefix, 'channels'].join('/'),
   channelPath: (id) => [host, prefix, 'channels', id].join('/'),
   channelMessagesPath: (id) => [host, prefix, 'channels', id, 'messages'].join('/'),
-  loginPath: () => [prefix, 'login'].join('/'),
+  loginPath: () => [host, prefix, 'login'].join('/'),
+  dataPath: () => [host, prefix, 'data'].join('/'),
 };
