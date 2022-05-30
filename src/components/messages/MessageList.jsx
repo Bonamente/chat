d8dc283b77664ed4a0346e6c23b380041628006b
Bonamente <56506035@protonmail.com>
2022-05-30 15:00:53 +0500

    feat: add socket prop to MessageList

--- src/components/messages/MessageList.jsx
+++ src/components/messages/MessageList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import MessageForm from './MessageForm.jsx';
 
-const MessageList = ({ currentChannel, currentMessages }) => (
+const MessageList = ({ currentChannel, currentMessages, socket }) => (
   <div className="col p-0 h-100">
     <div className="d-flex flex-column h-100">
       <div className="bg-light mb-4 p-3 shadow-sm small">
@@ -26,7 +26,7 @@ const MessageList = ({ currentChannel, currentMessages }) => (
         ))}
       </div>
       <div className="mt-auto px-5 py-3">
-        <MessageForm currentChannelId={currentChannel?.id} />
+        <MessageForm currentChannelId={currentChannel?.id} socket={socket} />
       </div>
     </div>
   </div>
