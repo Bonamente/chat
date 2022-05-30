7ba45af7ac6d6479632097bcad36484b65642fd4
Bonamente <56506035@protonmail.com>
2022-05-30 15:03:35 +0500

    feat: add message submit handler to MessageForm

--- src/components/messages/MessageForm.jsx
+++ src/components/messages/MessageForm.jsx
@@ -4,15 +4,33 @@ import { useFormik } from 'formik';
 
 import SendIcon from '../icons/SendIcon.jsx';
 
-const MessageForm = ({ currentChannelId }) => {
+const MessageForm = ({ currentChannelId, socket }) => {
   const inputRef = useRef();
 
   useEffect(() => {
     inputRef.current.focus();
   }, []);
 
-  // TODO
-  const onSubmit = () => { console.log(currentChannelId); };
+  const onSubmit = (values, { resetForm, setSubmitting }) => {
+    setSubmitting(true);
+
+    const message = {
+      author: localStorage.getItem('userName'),
+      text: values.message,
+      channelId: currentChannelId,
+    };
+
+    socket.emit('newMessage', message, (response) => {
+      if (response.status === 'ok') {
+        setSubmitting(false);
+        resetForm();
+        inputRef.current.focus();
+      } else {
+        // TODO
+        console.log('net error!');
+      }
+    });
+  };
 
   const formik = useFormik({
     initialValues: { message: '' },
@@ -31,12 +49,13 @@ const MessageForm = ({ currentChannelId }) => {
             aria-label="Новое сообщение"
             className="border-0 p-0 ps-2"
             ref={inputRef}
+            readOnly={formik.isSubmitting}
             {...formik.getFieldProps('message')}
           />
           <button
             type="submit"
             className="btn btn-group-vertical"
-            disabled={formik.values.message === ''}
+            disabled={formik.values.message === '' || formik.isSubmitting}
           >
             <SendIcon />
             <span className="visually-hidden">Отправить</span>
