34f4b1dc974dd0c3963cb7d6150d0302ab04584b
Bonamente <56506035@protonmail.com>
2022-06-02 17:43:25 +0500

    feat: add log out button logic

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -3,15 +3,21 @@ import React from 'react';
 import { Navbar, Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-const MainNavigation = () => (
-  <Navbar expand="lg" bg="white" variant="light" className="shadow-sm">
-    <Container>
-      <Navbar.Brand as={Link} to="/">
-        Hexlet Chat
-      </Navbar.Brand>
-      <Button>Выйти</Button>
-    </Container>
-  </Navbar>
-);
+import useAuth from '../../hooks/useAuth.jsx';
+
+const MainNavigation = () => {
+  const { loggedIn, logOut } = useAuth();
+
+  return (
+    <Navbar expand="lg" bg="white" variant="light" className="shadow-sm">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Hexlet Chat
+        </Navbar.Brand>
+        { loggedIn && <Button type="button" onClick={logOut}>Выйти</Button> }
+      </Container>
+    </Navbar>
+  );
+};
 
 export default MainNavigation;
