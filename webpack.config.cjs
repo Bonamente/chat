f3858ebfcf4eaede9e3f034128699f088c4f44fb
Bonamente <56506035@protonmail.com>
2022-06-07 13:00:26 +0500

    chore: update webpack.config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -41,6 +41,10 @@ module.exports = {
           { loader: 'sass-loader' },
         ],
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
 };
