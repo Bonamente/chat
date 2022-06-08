f1e9b604a8fab8fe2144a85053f809998006015e
Bonamente <56506035@protonmail.com>
2022-06-08 11:43:59 +0500

    feat: add error tracking with Rollbar

--- src/init.jsx
+++ src/init.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { io } from 'socket.io-client';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
@@ -29,15 +30,28 @@ const runApp = async () => {
       resources,
     });
 
+  const rollbarConfig = {
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      environment: 'production',
+    },
+  };
+
   const container = document.getElementById('chat');
   const root = createRoot(container);
 
   root.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App socket={socket} />
-      </BrowserRouter>
-    </Provider>,
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App socket={socket} />
+          </BrowserRouter>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>,
   );
 };
 
